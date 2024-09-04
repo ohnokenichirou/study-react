7f6efa1c228f1c0630eaf3852466f80267c3a2b7
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-04 19:12:54 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,35 @@ import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main';
 import { Footer } from 'src/components/Footer';
+import { useCallback } from 'react';
+
+// const handleClick = (e, foo) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo);
+// }
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+  
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a 
+        href='/about'
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
       <Main page="index"/>
       <Footer />
     </div>
