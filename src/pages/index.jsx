165dfb028e192b40412abe403339d5a426077714
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-04 18:17:31 +0900

    🏗️ 拡張子・ディレクトリ構成・絶対パスでのインポート方法に変更、_appを用いた処理共通化

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,14 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { Footer } from '../components/Footer';
-import { Main } from '../components/Main';
-import { Header } from '../components/Header';
+import styles from 'src/styles/Home.module.css';
+import { Header } from 'src/components/Header';
+import { Main } from 'src/components/Main';
+import { Footer } from 'src/components/Footer';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Main page="index"/>
