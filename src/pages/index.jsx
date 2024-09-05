ad88a83f0c3c2bbd20b86b5b91e4ba4dc044bfd4
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-05 19:16:10 +0900

    ♻️ 状態を変更するときにprevを付けて分かり易くした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,15 +12,15 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
-  const hendleChange = useCallback((e) => {
+  const handleChange = useCallback((e) => {
     if (e.target.value.length > 5) {
       alert("５文字以内にしてください");
       return;
