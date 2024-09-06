493037f7447357a872b9c393801c36ce2d5ec089
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-06 18:57:11 +0900

    ✨ ページ間のStateのリフトアップについて勉強

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/**  @type {import(‘next’).NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+};
+module.exports = nextConfig;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,15 +3,17 @@ import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
-export default function About() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleChange, handleAdd } = useInputArray();
-  useBgLightBlue();
-
+export default function About({
+  text,
+  array,
+  handleChange,
+  handleAdd,
+  count,
+  isShow,
+  handleClick,
+  handleDisplay,
+}) {
   return (
     <div className={styles.container}>
       <Head>
