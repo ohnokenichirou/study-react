4f9fda59952652c251a45f3b19f86aacefbf5388
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-09 15:42:33 +0900

    ✨ コンポーネント間のStateのリフトアップについて勉強

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,14 +1,15 @@
-import classes from 'src/components/Headline/Headline.module.css';
+import classes from "src/components/Headline/Headline.module.css";
 
 export function Headline(props) {
   return (
     <div>
-        <h1 className={classes.title}>{props.page} page</h1>
+      <h1 className={classes.title}>{props.page} page</h1>
 
-        <p className={classes.description}>
-          Get started by editing{' '}
-          {props.children}
-        </p>
+      <p className={classes.description}>
+        アイテムの数は {props.children} 個です
+      </p>
+
+      <button onClick={props.handleReduce}>減らす</button>
     </div>
   );
 }
